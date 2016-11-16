37f64224b9dd24c1b5d42eb46c486b9a486206a9
Brandon Lee <brandonlee503@gmail.com>
2016-11-15 16:54:01 -0800

    Init project 6

---
+++ ComputerGraphicsProject6/ComputerGraphicsProject6/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ComputerGraphicsProject6
+//
+//  Created by Brandon Lee on 11/15/16.
+//  Copyright © 2016 Brandon Lee. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
